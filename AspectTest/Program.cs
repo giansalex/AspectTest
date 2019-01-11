18b98d11346470453124d343e375070b21b57fd2
Giancarlos Salas <giansalex@gmail.com>
2019-01-11 16:32:23 -0500

    Agregar archivos de proyecto.

---
+++ AspectTest/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ArxOne.MrAdvice.Advice;
+
+namespace AspectTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyProudMethod();
+            Console.ReadKey();
+        }
+
+        [MyProudAdvice]
+        public static void MyProudMethod()
+        {
+            Console.WriteLine("Running Method");
+        }
+    }
+
+    public class MyProudAdvice : Attribute, IMethodAdvice
+    {
+        public void Advise(MethodAdviceContext context)
+        {
+            Console.WriteLine("Before Run Method");
+            // do things you want here
+            context.Proceed(); // this calls the original method
+            // do other things here
+        }
+    }
+
+
+}
